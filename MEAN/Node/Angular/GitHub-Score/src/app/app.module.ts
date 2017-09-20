f2ed8db394aaeb9eb2b39180a24c39fe97bd6f9f
HollinRoberts <hollinroberts@gmail.com>
2017-09-19 19:55:30 -0700

    GitHub Score almost complete

--- MEAN/Node/Angular/GitHub-Score/src/app/app.module.ts
+++ MEAN/Node/Angular/GitHub-Score/src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { TaskService } from './task.service';
+import { TaskService} from './task.service';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './task-list/task-list.component';
-import { TaskNewComponent } from './task-new/task-new.component';
-import { HttpModule} from '@angular/http';
+import { UserComponent } from './user/user.component';
+import { FormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent,
-    TaskNewComponent,
+    UserComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule,
     HttpModule,
+    FormsModule,
+    BrowserModule,
   ],
   providers: [TaskService],
   bootstrap: [AppComponent]
--- MEAN/Node/Angular/myFirstAngularApp/src/app/app.module.ts
+++ MEAN/Node/Angular/myFirstAngularApp/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { TaskService} from './task.service';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskNewComponent } from './task-new/task-new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskNewComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
