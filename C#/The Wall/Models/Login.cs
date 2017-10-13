8f19d2ab26d7504dec87a99781fe21ac61f5da51
HollinRoberts <hollinroberts@gmail.com>
2017-10-12 18:50:43 -0700

    the wall complete

---
+++ C#/The Wall/Models/Login.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginRegistration.Models
+{
+    public class Login
+    {
+        [Required]
+        [EmailAddress]
+        public string Email {get;set;}
+        [Required]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password {get;set;}
+       
+    }
+}
